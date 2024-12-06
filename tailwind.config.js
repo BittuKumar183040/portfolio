21236a2e672e244fced6be2044937cf52a900988
BittuKumar183040 <bk183040@gmail.com>
2024-12-06 13:54:32 +0530

    fix_required

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import textshadow from 'tailwindcss-textshadow';
+import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
@@ -9,7 +10,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require("daisyui"), 
+    daisyui, 
     textshadow
   ],
   daisyui: {
