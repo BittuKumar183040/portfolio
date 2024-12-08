4b74c6f5cae50a845e252268a61f7c12ffac1249
BittuKumar183040 <bk183040@gmail.com>
2024-12-08 07:06:27 +0530

    tooltip Added on project Btns

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,7 +12,7 @@ interface ProjectProps {
 
 const ProjectCard : React.FC<ProjectProps> = ({title, description, imageSource, gitLink, previewLink}) => {
   return (
-    <div className="card glass md:w-80 lg:w-96 w-full justify-around overflow-hidden transition-all shadow-sm hover:shadow-xl hover:-translate-y-2">
+    <div className="card glass md:w-80 lg:w-96 w-full justify-around transition-all shadow-sm hover:shadow-xl hover:-translate-y-2">
       <div className={`carousel select-none carousel-vertical hover "cursor-pointer" rounded-box h-56 flex flex-col bg-slate-200`}>
         <img
             className="carousel-item h-full w-full object-cover object-top pointer-events-none"
@@ -25,8 +25,15 @@ const ProjectCard : React.FC<ProjectProps> = ({title, description, imageSource, 
         <h2 className="card-title">{title}</h2>
         <div>{description}</div>    
         <div className="card-actions justify-end mt-4 flex gap-5">
-          { gitLink && <FaGithub className=' cursor-pointer' onClick={() => window.open(gitLink, "_blank")} size={25}/>}
-          { previewLink && <FaRegEye className=' cursor-pointer' onClick={() => window.open(previewLink, "_blank")} size={25}/>}
+          { gitLink && 
+            <div className=' tooltip tooltip-top' data-tip="Github">
+              <FaGithub className=' cursor-pointer' data-tip="Github" onClick={() => window.open(gitLink, "_blank")} size={25}/>
+            </div>
+          }
+          { previewLink && 
+          <div className=' tooltip tooltip-top' data-tip="Project Preview">
+            <FaRegEye className=' cursor-pointer' onClick={() => window.open(previewLink, "_blank")} size={25}/>
+          </div>}
         </div>
       </div>
     </div>
