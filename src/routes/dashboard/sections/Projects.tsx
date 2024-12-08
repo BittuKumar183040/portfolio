d0c1c2eafc7fe274ec5ebc2ae2903517ef53e603
BittuKumar183040 <bk183040@gmail.com>
2024-12-08 07:14:08 +0530

    advice Gen project Added

--- src/routes/dashboard/sections/Projects.tsx
+++ src/routes/dashboard/sections/Projects.tsx
@@ -2,12 +2,13 @@ import ProjectCard from '../../../components/ProjectCard';
 
 import ecom from '../../../assets/ecommerce/ecommerce1.jpg';
 import tic from '../../../assets/tictac/tic1.jpg';
+import adv from '../../../assets/advice/adv1.jpg';
 
 const Projects = () => {
   return (
     <section className=" container mx-auto mt-4 mb-10">
-        <h1 className="text-2xl md:text-2xl lg:text-3xl font-bold text-red-600 text-center text-shadow-md">Project</h1>
-        <h1 className="text-md md:text-md lg:text-xl font-bold text-red-600 text-center opacity-60 mb-6 text-shadow">List of all the project that I have build in past years.</h1>
+        <h1 className="text-2xl md:text-2xl lg:text-3xl font-bold text-red-600 text-center text-shadow-md">Projects</h1>
+        <h1 className="text-md md:text-md lg:text-xl font-bold text-red-600 text-center opacity-80 mb-6 text-shadow">List of all the project that I have build in past years.</h1>
         <div className=' flex gap-4 flex-wrap justify-around'>
 
           <ProjectCard 
@@ -24,6 +25,14 @@ const Projects = () => {
             imageSource={tic}
             gitLink="https://github.com/BittuKumar183040/tictac"
             previewLink="https://bittukumar183040.github.io/tictac"
+          />
+
+          <ProjectCard 
+            title='Advice Generator App' 
+            description="By using APIs, this app generates a random advice for you." 
+            imageSource={adv}
+            gitLink="https://github.com/BittuKumar183040/Advice-Generator"
+            previewLink="https://bittukumar183040.github.io/Advice-Generator/"
           />
 
         </div>
