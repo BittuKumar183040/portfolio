ef4bd493be1e53cebeac8f2c42bc260f13d08eac
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2025-02-14 01:07:47 +0530

    preserve selected project tab

--- src/routes/dashboard/sections/Projects.tsx
+++ src/routes/dashboard/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import ProjectCard from '../../../components/ProjectCard';
 import { projects } from '../../../assets/data/projects';
 import SectionHeading from '../../../components/SectionHeading';
+import { useState } from 'react';
 
 const ProjectsItems = ({ group }: any) => {
   const projectList = (key: string) => {
@@ -27,19 +28,29 @@ const ProjectsItems = ({ group }: any) => {
   );
 };
 
-const GroupProjects = () => {
+const GroupProjects = ({ activeTab }: { activeTab: string }) => {
+  const handleTabChange = (e: any) => {
+    const currentTab = e.target.ariaLabel.toLowerCase();
+    localStorage.setItem('currentTab', currentTab);
+  };
+
   return (
-    <div role="tablist" className="tabs tabs-lifted text-black dark:text-white">
+    <div
+      role="tablist"
+      className="tabs tabs-lifted dark:opacity-80"
+      onChange={handleTabChange}
+    >
       <input
         type="radio"
         name="my_tabs_2"
         role="tab"
-        className="tab"
+        className="tab dark:text-gray-500"
         aria-label="All"
+        defaultChecked={activeTab === 'all'}
       />
       <div
         role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 rounded-box md:p-6 p-2 pb-6"
+        className="tab-content bg-base-100 border-base-300 rounded-box md:p-6 p-2 pb-6 "
       >
         <ProjectsItems group="all" />
       </div>
@@ -48,9 +59,9 @@ const GroupProjects = () => {
         type="radio"
         name="my_tabs_2"
         role="tab"
-        className="tab"
+        className="tab dark:text-gray-500"
         aria-label="Major"
-        defaultChecked
+        defaultChecked={activeTab === 'major'}
       />
       <div
         role="tabpanel"
@@ -63,8 +74,9 @@ const GroupProjects = () => {
         type="radio"
         name="my_tabs_2"
         role="tab"
-        className="tab"
+        className="tab dark:text-gray-500"
         aria-label="Minor"
+        defaultChecked={activeTab === 'minor'}
       />
       <div
         role="tabpanel"
@@ -76,8 +88,9 @@ const GroupProjects = () => {
         type="radio"
         name="my_tabs_2"
         role="tab"
-        className="tab"
+        className="tab dark:text-gray-500"
         aria-label="Games"
+        defaultChecked={activeTab === 'games'}
       />
       <div
         role="tabpanel"
@@ -90,13 +103,14 @@ const GroupProjects = () => {
 };
 
 const Projects = () => {
+  const activeTab = localStorage.getItem('currentTab') || 'major';
   return (
     <section className=" container mx-auto mt-4 pb-10">
       <SectionHeading
         heading="Projects"
         subHeading="List of all the project that I have build in past years."
       />
-      <GroupProjects />
+      <GroupProjects activeTab={activeTab} />
     </section>
   );
 };
